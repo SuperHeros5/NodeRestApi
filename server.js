93261bc79aff12022746db9eb44890a43c6556f8
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-10-13 12:45:49 +0530

    sample response added
    sample response added

--- server.js
+++ server.js
@@ -69,7 +69,14 @@ console.log(req.body.name);
 
 			res.json({ message: 'Bear created!' });
 		});*/
-	res.json({ message: 'Bear created!' });
+	//res.json({ message: 'Bear created!' });
+	res.json({
+"speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": {},
+"contextOut": [],
+"source": "DuckDuckGo"
+});
 		
 	})
 
