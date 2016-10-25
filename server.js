42f8cc6ab329cbcc8bf5c4e2e4a4e0234d8cd784
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-10-25 16:02:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -106,12 +106,12 @@ router.route('/bears')
 				res.send(err);
 		
 	for (var i=0;i< all.length;i++) {
-		var element = "'"+all[i]+"'";
-		var jsonelem = JSON.parse(element);
+		var element = all[i];
+		
   var oneelemnt ={
-	   bear : jsonelem,
-            title:  "Place "+jsonelem['timings'],
-            "subtitle": "Landmark "+jsonelem.landmark,            
+	   bear : element,
+            title:  "Place "+element.place,
+            "subtitle": "Landmark "+element.landmark,            
 	   item_url: "https://www.oculus.com/en-us/rift/",
             image_url: "http://messengerdemo.parseapp.com/img/touch.png",
             buttons: [{
