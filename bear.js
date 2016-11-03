56437e3275b32e0a0f67e983da33776e0f41fc62
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-11-03 16:48:22 +0530

    from and to adibatla updated

--- bear.js
+++ bear.js
@@ -2,7 +2,19 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var BearSchema   = new Schema({
-	name: String
+	place: String,
+	landmark: String,
+	lattitude: String,
+	longitude: String,
+	timings: Array
 });
-
-module.exports = mongoose.model('busroutes', BearSchema);
+var BearSchema1   = new Schema({
+	place: String,
+	landmark: String,
+	lattitude: String,
+	longitude: String,
+	timings: Array
+});
+var toadibatla = mongoose.model('ToAdibatla', BearSchema,'ToAdibatla');
+var fromadibatla = mongoose.model('FromAdibatla', BearSchema1,'FromAdibatla');
+module.exports = { toadi : toadibatla , fromadi : fromadibatla};
