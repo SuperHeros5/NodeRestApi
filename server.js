f92ca0a98a666088000e4d8daad529802331f14d
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-11-11 12:10:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -95,11 +95,33 @@ router.route('/toadibatla')
 });
 	}else if(req.body.result.parameters.fromstop){
 		
-	Bear.toadi.findOne({ "place" :  new RegExp('^'+req.body.result.parameters.fromstop, "i")}, function(err, bear) {
+	Bear.toadi.find({ "place" :  new RegExp('^'+req.body.result.parameters.fromstop, "i")}, function(err, bear) {
 			if (err)
 				res.send(err);
 		if(bear != null){
 			
+			if(bear.length>1){
+				var buttonarray=[];
+				for(var i=0;i<bear.length;i++){
+				if(i === 3){break;}
+				buttonarray.push({
+				 "type":"postback",
+				    "title": bear[i].place,
+				    "payload":"TOADIBATLA "+bear[i].place
+				});
+				}
+			fbdata.push({
+			    "attachment":{
+			      "type":"template",
+			      "payload":{
+				"template_type":"button",
+				"text":"Do you really mean?",
+				"buttons":buttonarray
+			      }
+			      }
+			    });
+
+			}else{
 			
 	                fbdata.push({"text": bear.place+" to Adibatla"});
 			fbdata.push({"text":"Landmark: "+bear.landmark});
@@ -110,7 +132,7 @@ router.route('/toadibatla')
 		fbdata.push({"text":timing.time+"  "+getRoute(timing.routes)});
 		
 		});
-			}
+			}	
 		
 		}else{
 		fbdata=[{
@@ -127,9 +149,6 @@ router.route('/toadibatla')
 });
 		});
 	
-		/*fbdata={
-  	"text":"Entered from stop"+ req.body.result.parameters.fromstop
-    };*/
 	}else if(req.body.result.parameters.tostop){
 		
 		Bear.fromadi.findOne({ "place" :  new RegExp('^'+req.body.result.parameters.tostop, "i")}, function(err, bear) {
