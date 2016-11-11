88d6c901ee03bf243d0848d49959efdfebc30574
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-11-11 11:57:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -95,33 +95,12 @@ router.route('/toadibatla')
 });
 	}else if(req.body.result.parameters.fromstop){
 		
-	Bear.toadi.find({ "place" :  new RegExp('^'+req.body.result.parameters.fromstop, "i")}, function(err, bear) {
+	Bear.toadi.findOne({ "place" :  new RegExp('^'+req.body.result.parameters.fromstop, "i")}, function(err, bear) {
 			if (err)
 				res.send(err);
 		if(bear != null){
 			
-			if(bear.length>1){
-				var buttonarray=[];
-				for(var i=0;i<bear.length;i++){
-				if(i === 3){break;}
-				buttonarray.push({
-				 "type":"postback",
-				    "title": bear[i].place,
-				    "payload":"TOADIBATLA "+bear[i].place
-				});
-				}
-			fbdata.push({
-			    "attachment":{
-			      "type":"template",
-			      "payload":{
-				"template_type":"button",
-				"text":"Do you really mean?",
-				"buttons":buttonarray
-			      }
-			      }
-			    });
-
-			}else{
+			
 	                fbdata.push({"text": bear.place+" to Adibatla"});
 			fbdata.push({"text":"Landmark: "+bear.landmark});
 			fbdata.push({"text":"Timings  BusNumbers"});
