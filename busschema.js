21bf5c17ec5f20d7921172790bfca2c1926bf759
ksivaganesh99 <ksivaganesh99@gmail.com>
2016-11-18 13:44:33 +0530

    busschemas changed

--- busschema.js
+++ busschema.js
@@ -1,20 +1,20 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var BearSchema   = new Schema({
+var BusSchema1   = new Schema({
 	place: String,
 	landmark: String,
 	lattitude: String,
 	longitude: String,
 	timings: Array
 });
-var BearSchema1   = new Schema({
+var BusSchema2   = new Schema({
 	place: String,
 	landmark: String,
 	lattitude: String,
 	longitude: String,
 	timings: Array
 });
-var toadibatla = mongoose.model('ToAdibatla', BearSchema,'ToAdibatla');
-var fromadibatla = mongoose.model('FromAdibatla', BearSchema1,'FromAdibatla');
+var toadibatla = mongoose.model('ToAdibatla', BusSchema1,'ToAdibatla');
+var fromadibatla = mongoose.model('FromAdibatla', BusSchema2,'FromAdibatla');
 module.exports = { toadi : toadibatla , fromadi : fromadibatla};
